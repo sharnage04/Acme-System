f369876bfa305a3f4becb40e392ec35f473009f0
Samuel Harnage <sharnage04@gmail.com>
2025-08-27 20:30:46 -0400

    Fixed

--- acmesystem/src/ver1/HelloWorld.java
+++ acmesystem/src/ver1/HelloWorld.java
@@ -6,4 +6,8 @@ public class HelloWorld {
 		System.out.println("Hello from Eclipse");
 		System.out.println("Dave is working locally on his branch");
 	}
+	public static void heyNow() {
+		String Msg = "Hey Now!";
+		System.out.println(Msg);
+	}
 }
