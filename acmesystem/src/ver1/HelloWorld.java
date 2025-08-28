66ff1fa701a3998c90eb7540d2ac0ac15657dc84
Samuel Harnage <sharnage04@gmail.com>
2025-08-27 20:52:06 -0400

    Made whatFor method

--- acmesystem/src/ver1/HelloWorld.java
+++ acmesystem/src/ver1/HelloWorld.java
@@ -1,13 +1,25 @@
 package ver1;
+import java.util.Scanner;
 
 public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello from Eclipse");
 		System.out.println("Dave is working locally on his branch");
+	
+		Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter something: ");
+        String userInput = scanner.nextLine();
+
+        whatFor(userInput);
+
+        scanner.close();
 	}
 	public static void heyNow() {
 		String Msg = "Hey Now!";
 		System.out.println(Msg);
 	}
+	public static void whatFor(String stringArg) {
+        System.out.println(stringArg + ", what for?");
+    }
 }
