82e19c39fcdfa873756c53336948a736c0c5fc31
Samuel Harnage <sharnage04@gmail.com>
2025-08-27 20:00:03 -0400

    Added new method to HelloWorlld

--- acmesystem/src/ver1/HelloWorld.java
+++ acmesystem/src/ver1/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello from Eclipse");
+		System.out.println("This is a change made directly on GitHub");
 		System.out.println("Dave is working locally on his branch");
 	}
 	
